1086ab366765fe3c6558939aa032220e43699350
jarnalyrkar <joachimsolstrom@gmail.com>
2022-05-11 13:29:35 +0200

    Add help text

--- resources/javascript/site.js
+++ resources/javascript/site.js
@@ -1,5 +1,6 @@
 import MenuButtonlistener from './modules/navigation/MenuButtonListener'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Add all your JS modules here:
   let menuButtonListener = new MenuButtonlistener()
 })
